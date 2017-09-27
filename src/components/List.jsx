13445ea1c40053b262ba95a43da0571f6d5dbfaf
jamikk <jkarimov1593@gmail.com>
2017-09-27 11:40:54 +0300

    try create delete action

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,21 +3,17 @@ import Input from './Input';
  
 
 export default function List(props) {
-    // let template;
-    // const data = this.props.dataItems;
 
-    // if (this.props.dataItem) {
-        let template = this.props.dataItem.map((item, i) => {
-            return (
-                <Input 
-                    value={item.textValue} 
-                    key={item.id} 
-                    className="list__item" 
-                    index={i} 
-                />
-            )
-        })
-    // }
+    let template = this.props.dataItem.map((item, i) => {
+        return (
+            <Input 
+                value={item.textValue} 
+                key={item.id} 
+                className="list__item" 
+                index={i} 
+            />
+        )
+    })
 
     return (
         <div className="list">
